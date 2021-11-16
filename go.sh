14eaa514e210ba02c8b6b1d8b775b6e352b66bc6
Jack Bauerle <jackbauerle@Jacks-Mac-mini.hsd1.ma.comcast.net>
2021-11-15 19:45:57 -0500

    beautiful

---
+++ go.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while [ 1 -lt 2 ]
+do
+  flexget execute;
+  mv torrents/* "/Volumes/TOSHIBA EXT/torrents";
+  sleep 1800;
+done
+
---
+++ temp/transmission.sh
@@ -0,0 +1,3 @@
+brew install transmission;
+transmission-daemon --download-dir "/Volumes/TOSHIBA EXT/tv"
+transmission-daemon -c /Users/jackbauerle/code/tvfeed/torrents
